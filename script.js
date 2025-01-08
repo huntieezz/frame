1ee2e29a7b0e50495626be4a4af759d2b1946be4
Huntiezz <huntieezz@outlook.com>
2025-01-08 12:33:06 +0100

    Added Navigation Bar

--- script.js
+++ script.js
@@ -3,11 +3,3 @@ document.querySelectorAll('.frame').forEach(frame => {
         frame.classList.toggle('enlarged');
     });
 });
-
-function showCredits() {
-    document.getElementById('credits-modal').style.display = 'block';
-}
-
-function hideCredits() {
-    document.getElementById('credits-modal').style.display = 'none';
-}
